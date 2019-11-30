fdde1aa76f6b10462be007d2e48a2b292188fdda
Rade <narancic.rideen@gmail.com>
2019-11-30 20:44:56 +0100

    Platform detection, Landscape mode enabled and adjusted

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    Platform
+} from 'react-native';
 
 import Colors from '../constants/Colors';
 
 const MainButton = props => {
+    let ButtonComponent = TouchableOpacity;
+
+    // Android version 21 and higher support ripple effect
+    // Platform check can be avoided by creating MainButton.android.js & MainButton.ios.js 
+    // and React Native will load them accordingly (avoid 'android.js' and 'ios.js' in import declarations)
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback;
+    }
+
     return (
-        <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-            <View style={styles.button}>
-                <Text style={styles.buttonText}>{props.children}</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+            <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>{props.children}</Text>
+                </View>
+            </ButtonComponent>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    buttonContainer: {
+        borderRadius: 25,
+        overflow: 'hidden'
+    },
     button: {
         backgroundColor: Colors.primary,
         paddingVertical: 12,
         paddingHorizontal: 30,
-        borderRadius: 25 
+        borderRadius: 25
     },
     buttonText: {
         color: 'white',
